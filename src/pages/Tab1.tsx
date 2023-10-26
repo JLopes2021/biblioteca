b7bd76e71465fb8921fb58d28c3170b394c9fb0b
JLopes2021 <jeffinho.je8@gmail.com>
2023-10-26 11:55:47 -0300

    busca-livro

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,48 +1,73 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonButtons, IonMenuButton, IonButton, IonIcon } from '@ionic/react';
 import { useHistory } from 'react-router-dom';
 import books from './BooksData'; // Importe a constante books
 import './Tab1.css';
+import { search, person } from 'ionicons/icons'; // Importando os ícones
 
 const Tab1: React.FC = () => {
   const history = useHistory();
+  const [searchTerm, setSearchTerm] = useState('');
+  const sortedBooks = [...books].sort((a, b) => a.title.localeCompare(b.title));
 
   const goToReadBook = (bookId: string) => {
     history.push(`/ler-livro/${bookId}`);
   }
-  const sortedBooks = [...books].sort((a, b) => a.title.localeCompare(b.title));
 
-  
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar color="primary">
+          <IonButtons slot="start">
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
           <IonTitle>Biblioteca</IonTitle>
+          <IonButtons slot="end">
+              <div className="search-container">
+                  <input
+                    type="text"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Buscar livros..."
+                  />
+                </div>
+            <IonButton routerLink="/profile">
+              <IonIcon slot="icon-only" icon={person}></IonIcon>
+            </IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
+
       <IonContent>
-      <div className="content">
-        <IonGrid>
-          <IonRow>
-            <IonCol size="12">
-              <ul>
-                {sortedBooks.map(book => (
-                  <li key={book.id} onClick={() => goToReadBook(book.id)} className="book">
-                    <div>
-                      <div className="thumbnail-container">
-                        <img src={book.image} alt={book.title} />
-                      </div>
-                      <div className="title">{book.title}</div>
-                      <div className="author">{book.author}</div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
-      </div>
-    </IonContent>    </IonPage>
+        <div className="content">
+          <IonGrid>
+            <IonRow>
+              <IonCol size="12">
+               
+                <ul>
+                  {sortedBooks
+                    .filter(book =>
+                      book.title.toLowerCase().includes(searchTerm.toLowerCase())
+                    )
+                    .map(book => (
+                      <li key={book.id} onClick={() => goToReadBook(book.id)} className="book">
+                        <div>
+                          <div className="thumbnail-container">
+                            <img src={book.image} alt={book.title} />
+                          </div>
+                          <div className="title">{book.title}</div>
+                          <div className="author">{book.author}</div>
+                        </div>
+                      </li>
+                    ))
+                  }
+                </ul>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </div>
+      </IonContent>
+    </IonPage>
   );
 };
 
